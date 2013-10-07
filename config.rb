835dd1e11c566105d78b311763aca39f8e9dfa9a
Andrew Hathaway <andrew@andrewhathaway.net>
2013-10-07 13:24:45 +0100

    Basic structure - HTML, CSS

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+line_comments = false
+# output_style = :expanded or :nested or :compact or :compressed
+# relative_assets = true
