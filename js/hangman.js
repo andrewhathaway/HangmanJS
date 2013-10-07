6fb368ac4311299aa39a2fe0be4745e4ff072b8b
Andrew Hathaway <andrew@andrewhathaway.net>
2013-10-07 13:49:30 +0100

    BG, more

--- js/hangman.js
+++ js/hangman.js
@@ -2,16 +2,31 @@
 
 	var HangmanJS = function() {
 
+		/**
+		 * Make sure that we're a constructor and not a function call
+		 */
 		if ((!this instanceof HangmanJS)) {
 			return new HangmanJS();
 		}
 
+		/**
+		 * Default words to pick from
+		 */
+		this.words = [
+			'technology',
+			'fantastic'
+		];
+
+		/**
+		 * Initiate HangmanJS
+		 */
 		this.init = function() {
-			alert($);
-		}
+
+		};
 
 	}
 
 	var hangman = new HangmanJS();
+	hangman.init();
 
 })($);
